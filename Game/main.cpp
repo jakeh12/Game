e282539a0fb85641ca174c6c26bbaa4724add46e
Jakub Hladik <jhladik@iastate.edu>
2018-01-01 20:51:44 +0100

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by Jakub Hladik on 1/1/18.
+//  Copyright © 2018 Jakub Hladik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
